20147a32ee583e18c40c6ae8dcff421b2d541377
sankassio99 <kciosantana@gmail.com>
2022-09-08 20:07:39 -0300

    #chore: host name of link zap

--- components/Molecules/buttonZap.vue
+++ components/Molecules/buttonZap.vue
@@ -1,11 +1,11 @@
 <template>
-    <button
-      class="base-button bg-green-400 py-4 w-full rounded-full hover:bg-green-500 flex gap-2 items-center justify-center"
-      @click="shareEncoded"
-    >
-        <span class="text-white font-semibold">Compartilhar no Zap</span>
-        <img src="/icons8-whatsapp.svg" alt="">
-    </button>
+  <button
+    class="base-button bg-green-400 py-4 w-full rounded-full hover:bg-green-500 flex gap-2 items-center justify-center"
+    @click="shareEncoded"
+  >
+    <span class="text-white font-semibold">Compartilhar no Zap</span>
+    <img src="/icons8-whatsapp.svg" alt="" />
+  </button>
 </template>
 
 <script>
@@ -17,10 +17,13 @@ export default {
   },
   methods: {
     shareEncoded() {
-        let headText = "*NÃO SE DEIXE ENGANAR: veja os verdadeiros números da CORRIDA PRESIDENCIAL DE 2022*";
+      let headText =
+        '*NÃO SE DEIXE ENGANAR: veja os verdadeiros números da CORRIDA PRESIDENCIAL DE 2022*'
       let text =
         '_Fácil e rápido, dê sua intenção de voto em menos de 1 minuto e veja o resultado em tempo real_'
-      let content = encodeURIComponent(headText + '\n\n' + text + '\n\n'  + window.location.href)
+      let content = encodeURIComponent(
+        headText + '\n\n' + text + '\n\n' + window.location.host
+      )
       window.open(`https://api.whatsapp.com/send?text=${content}`)
     },
   },
