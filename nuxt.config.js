7fa3b2d8c816e3b54e8970ed17b97517a05ecf45
sankassio99 <kciosantana@gmail.com>
2022-09-08 20:50:56 -0300

    #chore: description

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Veja o resultado das pesquisas presidenciais em tempo real' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
