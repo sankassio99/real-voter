08019fe34ae3ba8105054b36d02340dd6f8030d1
sankassio99 <kciosantana@gmail.com>
2022-09-08 20:18:58 -0300

    #bug: link to site

--- components/Molecules/buttonZap.vue
+++ components/Molecules/buttonZap.vue
@@ -22,7 +22,7 @@ export default {
       let text =
         '_Fácil e rápido, dê sua intenção de voto em menos de 1 minuto e veja o resultado em tempo real_'
       let content = encodeURIComponent(
-        headText + '\n\n' + text + '\n\n' + window.location.host
+        headText + '\n\n' + text + '\n\n' +  window.location.origin
       )
       window.open(`https://api.whatsapp.com/send?text=${content}`)
     },
