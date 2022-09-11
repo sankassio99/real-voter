432858d75c290060461f2c67d666eed6b4abe889
sankassio99 <kciosantana@gmail.com>
2022-09-11 15:07:40 -0300

    #feature: count total of votes

--- components/Organims/BarChart.vue
+++ components/Organims/BarChart.vue
@@ -1,19 +1,91 @@
 <script>
 import { Bar } from 'vue-chartjs'
-import ChartDataLabels from 'chartjs-plugin-labels';
+import ChartDataLabels from 'chartjs-plugin-labels'
 
 export default {
   extends: Bar,
-  props: ['data', 'options'],
+  props: ['data', 'options', 'totalOfVotes'],
   components: { ChartDataLabels },
   mounted() {
-    setTimeout(()=>{
-      this.renderChart(this.data, this.options)
-    }, 1000);
-  },
-  methods:{
-    updateChart(){
+    let barChartOptions = {
+      plugins: {
+        labels: {
+          render: 'image',
+          images: [
+            {
+              src: '/candidates/jair-bolsonaro.jpeg',
+              width: 35,
+              height: 35,
+            },
+            {
+              src: '/candidates/ciro-gomes-.jpeg',
+              width: 35,
+              height: 35,
+            },
+            {
+              src: '/candidates/felipe.jpeg',
+              width: 35,
+              height: 35,
+            },
+            {
+              src: '/candidates/lula-foto-tse.jpeg',
+              width: 35,
+              height: 35,
+            },
+            {
+              src: '/candidates/simone-tebet.jpeg',
+              width: 35,
+              height: 35,
+            },
+            {
+              src: '/candidates/soraya-thronicke-1.jpeg',
+              width: 35,
+              height: 35,
+            },
+          ],
+        },
+      },
+      responsive: true,
+      legend: {
+        display: true,
+      },
+      title: {
+        display: true,
+        text: `Total de votos: ${this.totalOfVotes}`,
+        fontSize: 14,
+        fontColor: '#6b7280',
+      },
+      tooltips: {
+        backgroundColor: '#17BF62',
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+              max: 360,
+              min: 0,
+              stepSize: 72,
+            },
+            gridLines: {
+              display: true,
+            },
+          },
+        ],
+      },
     }
-  }
+
+    this.renderChart(this.data, barChartOptions)
+  },
+  methods: {
+    updateChart() {},
+  },
 }
 </script>
--- pages/results.vue
+++ pages/results.vue
@@ -8,11 +8,18 @@
 
       <div class="max-w-xl">
         <bar-chart
+          v-if="dataIsGot"
           ref="bar"
           :data="barChartData"
           :options="barChartOptions"
-          :height="400"
+          :height="390"
+          :totalOfVotes="totalVotes"
         />
+        <div v-else class="h-96 flex flex-col items-center justify-center">
+          <p class="text-center font-semibold text-gray-600 animate-pulse">
+            Gerando gráfico ...
+          </p>
+        </div>
       </div>
 
       <div class="w-full flex flex-col justify-center items-center gap-2">
@@ -39,95 +46,30 @@ export default {
   data() {
     return {
       barChartData: null,
-      barChartOptions: {
-        plugins: {
-          labels: {
-            render: 'image',
-            images: [
-              {
-                src: '/candidates/jair-bolsonaro.jpeg',
-                width: 35,
-                height: 35,
-              },
-              {
-                src: '/candidates/ciro-gomes-.jpeg',
-                width: 35,
-                height: 35,
-              },
-              {
-                src: '/candidates/felipe.jpeg',
-                width: 35,
-                height: 35,
-              },
-              {
-                src: '/candidates/lula-foto-tse.jpeg',
-                width: 35,
-                height: 35,
-              },
-              {
-                src: '/candidates/simone-tebet.jpeg',
-                width: 35,
-                height: 35,
-              },
-              {
-                src: '/candidates/soraya-thronicke-1.jpeg',
-                width: 35,
-                height: 35,
-              },
-            ],
-          },
-        },
-        responsive: true,
-        legend: {
-          display: true,
-        },
-        title: {
-          display: true,
-          text: 'Total de votos: 460.000',
-          fontSize: 14,
-          fontColor: '#6b7280',
-        },
-        tooltips: {
-          backgroundColor: '#17BF62',
-        },
-        scales: {
-          xAxes: [
-            {
-              gridLines: {
-                display: false,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                max: 360,
-                min: 0,
-                stepSize: 72,
-              },
-              gridLines: {
-                display: true,
-              },
-            },
-          ],
-        },
-      },
+      totalVotes: 0,
+      dataIsGot: false,
       documents: [],
     }
   },
-  async mounted() {
+  computed: {},
+  async created() {
+    // todo: precisa ser refatorado
     let allDocs = await this.getAllDocs()
     let labels = []
     let values = []
+    // get candidates names
     allDocs.forEach((doc) => {
       labels.push(doc.id)
     })
+    // get candidate votes
     allDocs.forEach(async (doc) => {
-      let data = await this.getCadidateVotes(doc.id)
-      let multVotes = data * 3;
+      let votesCount = await this.getCadidateVotes(doc.id)
+      let multVotes = votesCount * 3
+      this.totalVotes += multVotes
       values.push(multVotes)
     })
+
+    // organize chart infos
     this.barChartData = {
       labels: labels,
       datasets: [
@@ -138,8 +80,19 @@ export default {
         },
       ],
     }
+
+    setTimeout(() => {
+      this.dataIsGot = true
+    }, 1000)
   },
-  methods: {},
+  methods: {
+    // updateGraphc() {
+    //   this.$nextTick(() => {
+    //     // Add the component back in
+    //     this.dataIsGot = false
+    //   })
+    // },
+  },
   head() {
     return {
       title: 'Real Eleitor',
