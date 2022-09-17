728b86870a3e6f9f4ae9966c788a0a444c5be51b
sankassio99 <kciosantana@gmail.com>
2022-09-17 16:27:33 -0300

    #chore: adjusts bar chart

--- components/Molecules/votesBarGoals.vue
+++ components/Molecules/votesBarGoals.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-20">
+  <div class="w-full px-4">
     <div class="animate-pulse rounded-full bg-blue-200 w-full h-5 flex items-center justify-between py-3 px-1">
       <div class="rounded-full bg-blue-800 h-5 flex items-center px-10" :class="'w-1/3'">
         <span class="text-white">{{votesQuantity}}</span>
